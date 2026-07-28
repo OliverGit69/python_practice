54a7d6b0ee4309f44017e82823a264d8c5271953
Sid <olivegit69@gmail.com>
2026-07-28 21:23:31 +0530

    Update test_programs.py

--- test_programs.py
+++ test_programs.py
@@ -1,13 +1,11 @@
-# Multiplication table (1 to 10) without using *
+# Ask the user for a number and determine whether it is a prime number using a loop.sx
 
-for i in range(1, 11):
-    for j in range(1, 11):
-        product = 0
-
-        # Multiply i and j using repeated addition
-        for _ in range(j):
-            product += i
-
-        print(f"{product:4}", end="")
-    print()
-    
+num = int(input("Please Enter a Number: "))
+if num <= 1:
+    print(f"Number {num} is Not a Prime")
+for i in range(1, num + 1):
+    if i != 1 and i != num and num % i == 0:
+        print(f"{num} is Not a Prime Number")
+        break
+else:
+   print(f"{num} is a Prime Number")
